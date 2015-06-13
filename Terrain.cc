627273e3ed2833d497e14abe2e46f89f105d5e1b
Seungwon Park <zezeon@Debian8.iptime.org>
2015-06-13 21:39:08 +0900

    xcf

--- Terrain.cc
+++ Terrain.cc
@@ -10,67 +10,6 @@
 #include <cstdlib>
 #include <ifstream>
 
-Terrain::Terrain() {
-	movePenaltyVsInfantry = 0;
-	movePenaltyVsArmor = 0;
-	movePenaltyVsShip = 100;
-	evadeBonus = 0;
-}
-
-City::City()
-{
-	movePenaltyVsInfantry = 0;
-	movePenaltyVsArmor = 0;
-	movePenaltyVsShip = 100;
-	evadeBonus = 20;
-}
-
-Harbor::Harbor()
-{
-	movePenaltyVsShip = 0;
-	evadeBonus = 0;
-}
-
-Sea::Sea()
-{
-	movePenaltyVsInfantry = 100;
-	movePenaltyVsArmor = 100;
-	movePenaltyVsShip = 0;
-}
-
-Forest::Forest()
-{
-	movePenaltyVsInfantry = 20;
-	movePenaltyVsArmor = 50;
-	evadeBonus = 30;
-}
-
-Hill::Hill()
-{
-	movePenaltyVsInfantry = 30;
-	movePenaltyVsArmor = 30;
-	evadeBonus = 20;
-}
-
-Mountain::Mountain()
-{
-	movePenaltyVsInfantry = 50;
-	movePenaltyVsArmor = 100;
-	evadeBonus = 60;
-}
-
-Desert::Desert()
-{
-	movePenaltyVsInfantry = 60;
-	movePenaltyVsArmor = 20;
-}
-
-Swamp::Swamp()
-{
-	movePenaltyVsInfantry = 50;
-	movePenaltyVsArmor = 70;
-	evadeBonus = 10;
-}
 
 Map::create_terrain_bitmap(char *filename)
 {
