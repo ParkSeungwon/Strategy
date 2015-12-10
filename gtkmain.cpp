dcc0ef4d5d2233d9516da584f236d0d2fe46aae2
ParkSeungwon <zezeon1@gmail.com>
2015-12-10 19:59:10 +0900

    modified:   gtkmain.cpp 	modified:   gtkmain.hpp 	modified:   main.cpp 	modified:   war

--- gtkmain.cpp
+++ gtkmain.cpp
@@ -1,12 +1,30 @@
+#include <cairomm/context.h>
 #include "gtkmain.hpp"
 
 GtkMain::GtkMain()
 {
-	//context = area.get_window()->create_cairo_context();
 	add(hbox1);
-	//vbox1.pack_start(menu, Gtk::PACK_SHRINK);
-	//vbox1.pack_start(hbox1);
-	//hbox1.pack_start(area);
-	//hbox1.pack_start(vbox2, Gtk::PACK_SHRINK);
+	vbox1.pack_start(menu, Gtk::PACK_SHRINK);
+	vbox1.pack_start(hbox1);
+	hbox1.pack_start(scrolled_win);
+	scrolled_win.add(area);
+	hbox1.pack_start(vbox2, Gtk::PACK_SHRINK);
 	show_all_children();
 }
+
+bool Draw_Area::on_draw()
+{
+	Gtk::Allocation allocation = get_allocation();
+	context->set_line_width(0.05);
+	context->move_to(0,0);
+	context->line_to(100,100);
+	context->stroke();
+	return true;
+}
+
+Draw_Area::Draw_Area() 
+{
+	context = get_window()->create_cairo_context();
+}
+
+Draw_Area::~Draw_Area() {}
